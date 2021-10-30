ba762f81192b10fe949155b1b910160112fd44d2
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-10-30 19:50:51 -0400

    add: edit, remove butons for each task

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,26 @@
 import React, { useState, useEffect } from 'react';
 
 function Home() {
-  const [inputTaskValue, setInputTaskValue] = useState('');
+  const [inputTask, setInputTask] = useState('');
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
 
-  }, [inputTaskValue, tasks]);
+  }, [inputTask, tasks]);
 
-  const handleClick = (target) => {
-    const { id } = target;
-    console.log(id)
-    setTasks([...tasks, inputTaskValue]);
+  const addTask = () => {
+    setTasks([...tasks, inputTask]);
+    setInputTask('');
+    console.log(inputTask)
   }
+
+  const removeTask = (id) => {
+    console.log('remove tafera ', id)
+  };
+
+  const editTask = (id) => {
+    console.log('edita tarefa ', id)
+  };
 
   return (
     <>
@@ -22,32 +30,38 @@ function Home() {
             type="text"
             id="task-input"
             placeholder="Type your task here"
-            onChange={(e) => setInputTaskValue(e.target.value)}
+            onChange={(e) => setInputTask(e.target.value)}
           />
         </label>
         <button
           type="button"
           id="add-task-button"
-          onClick={(e) => handleClick(e.target)}
+          onClick={addTask}
         >
           Add task
-        </button>
-        <button
-          type="button"
-          id="edit-task-button"
-        >
-          Edit task
-        </button>
-        <button
-          type="button"
-          id="remove-task-button"
-        >
-          remove task
-        </button>
+        </button>        
       </section>
       <section>
         <ul id="tasks">
-          { tasks.map((task, index) => <li key={index}>{task}</li>) }
+          { tasks.map((task, index) => (
+            <div key={index}>
+              <li>{task}</li>
+              <button
+                type="button"
+                id="edit-button"
+                onClick={() => editTask(index)}
+              >
+                Edit task
+              </button>
+              <button
+              type="button"
+              id="remove-button"
+              onClick={() => removeTask(index)}
+              >
+                remove task
+              </button>
+            </div>
+          ))}
         </ul>
       </section>
     </>
