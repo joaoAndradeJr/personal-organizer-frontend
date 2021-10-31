5b935f61a4b1b5e1dad46daa7a24cba151c87cfc
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-10-30 20:48:51 -0400

    add: fetch backend api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,22 @@ import React, { useState, useEffect } from 'react';
 function Home() {
   const [inputTask, setInputTask] = useState('');
   const [tasks, setTasks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-
-  }, [inputTask, tasks]);
+    setIsLoading(true);
+    fetch('http://localhost:3001/')
+      .then(resp => resp.json())
+      .then(data => {
+        setTasks(data);
+        setIsLoading(false);
+      });
+  }, [inputTask]);
 
   const addTask = () => {
-    setTasks([...tasks, inputTask]);
-    setInputTask('');
-    console.log(inputTask)
+    // setTasks([...tasks, inputTask]);
+    // setInputTask('');
+    // console.log(inputTask)
   }
 
   const removeTask = (id) => {
@@ -23,49 +30,54 @@ function Home() {
   };
 
   return (
-    <>
-      <section>
-        <label htmlFor="task-input">
-          <input
-            type="text"
-            id="task-input"
-            placeholder="Type your task here"
-            onChange={(e) => setInputTask(e.target.value)}
-          />
-        </label>
-        <button
-          type="button"
-          id="add-task-button"
-          onClick={addTask}
-        >
-          Add task
-        </button>        
-      </section>
-      <section>
-        <ul id="tasks">
-          { tasks.map((task, index) => (
-            <div key={index}>
-              <li>{task}</li>
-              <button
-                type="button"
-                id="edit-button"
-                onClick={() => editTask(index)}
-              >
-                Edit task
-              </button>
-              <button
+    <div>
+      { isLoading ? <h3>Loading...</h3>
+      : 
+        <>
+          <section>
+            <label htmlFor="task-input">
+              <input
+                type="text"
+                id="task-input"
+                placeholder="Type your task here"
+                onChange={(e) => setInputTask(e.target.value)}
+              />
+            </label>
+            <button
               type="button"
-              id="remove-button"
-              onClick={() => removeTask(index)}
-              >
-                remove task
-              </button>
-            </div>
-          ))}
-        </ul>
-      </section>
-    </>
-  );
+              id="add-task-button"
+              onClick={addTask}
+            >
+              Add task
+            </button>        
+          </section>
+          <section>
+            <ul>
+              { tasks.map((e, index) => (
+                <div key={index}>
+                  <li>{e.task}</li>
+                  <button
+                    type="button"
+                    id="edit-button"
+                    onClick={() => editTask(e._id)}
+                  >
+                    Edit task
+                  </button>
+                  <button
+                  type="button"
+                  id="remove-button"
+                  onClick={() => removeTask(e._id)}
+                  >
+                    remove task
+                  </button>
+                </div>
+              ))}
+            </ul>
+          </section>
+        </>
+      }
+    </div>
+  ); 
 }
 
 export default Home;
