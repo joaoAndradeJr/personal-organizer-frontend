ba7ed5f6aa6fea37e38c38bf440497f5551a1968
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-11-02 17:53:18 -0400

    add: React Router and configure index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import  { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import EditTask from './pages/EditTask';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <Home />
+    <Switch>
+      <Route exact path="/" component={ Home } />
+      <Route path="/edit" component={ EditTask } />
+      <Route path='*' component={ NotFound } />
+    </Switch>
   );
 }
 
