298b8af9870a9b62397259ccef6a92c3027ad7d4
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-11-03 22:01:49 -0400

    fix: change url in fetch line 65 and line 84

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function Home() {
   const bdUrl = 'https://personaltaskslist-bk.herokuapp.com';
@@ -60,7 +62,7 @@ function Home() {
     const status = document.querySelector('#task-status').value;
     const id = document.querySelector('#task-id').innerText.substr(4, 24);
     setIsLoading(true);
-    await fetch(`http://localhost:3001/${id}`, {
+    await fetch(`https://personaltaskslist-bk.herokuapp.com/${id}`, {
       method: 'PUT',
       headers: {
         Accept: 'application/json',
@@ -79,7 +81,7 @@ function Home() {
   const editTask = async (id) => {
     setIsLoading(true);
     setIsEditing(true);
-    const task = await fetch(`http://localhost:3001/${id}`, {
+    const task = await fetch(`https://personaltaskslist-bk.herokuapp.com/${id}`, {
       method: 'GET',
       headers: {
         Accept: 'application/json',
@@ -102,36 +104,49 @@ function Home() {
       : 
         <>
           <section>
-            <label htmlFor="task-input">
-              <input
-                type="text"
-                id="task-input"
-                placeholder="Escreva sua tarefa aqui..."
-                onChange={(e) => setInputTask(e.target.value)}
-              />
-            </label>
-            <select id="task-status" value={taskStatus} onChange={(e) => setTaskStatus(e.target.value)}>
-              <option value={PENDING}>Pendente</option>
-              <option value={IN_PROGRESS}>Em Andamento</option>
-              <option value={DONE}>Pronto</option>
-            </select>
-            { isEditing ?
-                <button
-                type="button"
-                id="edit-task-button"
-                onClick={saveTask}
-                >
-                  Save task
-                </button>  
-              :
-                <button
-                  type="button"
-                  id="add-task-button"
-                  onClick={addTask}
-                >
-                  Add task
-                </button>
-            }
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Label>
+                  Digite a tarefa:
+                  <Form.Control
+                    id="task-input"
+                    type="text"
+                    placeholder="Escreva sua tarefa aqui..."
+                    onChange={(e) => setInputTask(e.target.value)}
+                  />
+                </Form.Label>
+                <Form.Label>
+                  Selecione o status da tarefa:
+                  <Form.Control
+                    as="select"
+                    id="task-status"
+                    value={taskStatus}
+                    onChange={(e) => setTaskStatus(e.target.value)}
+                  >
+                    <option value={PENDING}>Pendente</option>
+                    <option value={IN_PROGRESS}>Em Andamento</option>
+                    <option value={DONE}>Pronto</option>
+                  </Form.Control>
+                </Form.Label>
+                { isEditing ?
+                  <Button
+                    variant="outline-primary"
+                    id="edit-task-button"
+                    onClick={saveTask}
+                  >
+                    Save task
+                  </Button>  
+                :
+                  <Button
+                    variant="outline-success"
+                    id="add-task-button"
+                    onClick={addTask}
+                  >
+                    Add task
+                  </Button>
+              }
+              </Form.Group>
+            </Form>
           </section>
           <section>
             { tasks.length > 0 ? 
